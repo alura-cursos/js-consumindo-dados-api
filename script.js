e4d23da7866662472b8d7843cf198674ee473eed
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-12-03 10:39:41 -0300

    aula 04

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
     try {
-        let consultaCEP = await fetch('https://viacep.com.br/ws/01001250/json/');
+        let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         let consultaCEPConvertida = await consultaCEP.json();
         if(consultaCEPConvertida.erro) {
             throw Error('CEP não existente!');
         }
         console.log(consultaCEPConvertida);
-
+        return consultaCEPConvertida;
     } catch (erro) {
         console.log(erro);
     }
 }
 
+    //aula 02
     // .then(resposta => resposta.json())
     //     .then(r => {
     //         if (r.erro) {
@@ -22,4 +23,10 @@ async function buscaEndereco() {
     //     }).catch(erro => console.log(erro))
     //         .finally(mensagem => console.log('Processamento concluído!'));
 
-buscaEndereco()
+
+
+//aula 04
+// let ceps = ['01001000', '01001001'];
+// let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+// console.log(conjuntoCeps)
+// Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
